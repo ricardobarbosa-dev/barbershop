957fadfaadfd22ee42e4c532d378cae18b337627
Ricardo Barbosa <157282194+ricardobarbosa-dev@users.noreply.github.com>
2026-01-24 11:12:41 -0300

    added modal

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ window.addEventListener('scroll', () => {
         navbar.classList.remove('scrolled');
     }
 });
+
+// modal
+const openBtn = document.getElementById("openPrices");
+const modal = document.getElementById("pricesModal");
+const closeBtn = document.getElementById("closePrices");
+
+openBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    modal.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    modal.classList.remove("active");
+});
+
+modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        modal.classList.remove("active");
+    }
+});
+
